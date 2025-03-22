52e643ac20eaf98e01effadd7f7b97b15e901097
rasa9521 <rsrasasingh@gmail.com>
2025-03-22 12:35:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = "21179966"
-api_hash = "d97919fb0a3c725e8bb2a25bbb37d57c"
-bot_token = "6796353747:AAGhACxmk8It_apZdjIb8VfjFj_Bl8S8Qm8"
-auth_users = "7326397503"
+api_id = "25364269"
+api_hash = "ddfbbd94cf441e22ee71bb7f4695c2f1"
+bot_token = "7906984827:AAHK9VXO1oDKCD3f-HfJlL8xnWF4wuE0wiM"
+auth_users = "2093417522"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "7326397503"
+osowner_users = "2093417522"
 owner_users = [int(num) for num in osowner_users.split(",")]
